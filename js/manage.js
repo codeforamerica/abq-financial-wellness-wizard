e1fe3ad6bfaea3270ec8591b2dc1ff466881610e
Becky Boone <becky.sue.boone@gmail.com>
2015-07-31 15:52:43 -0700

    Add stress
    Also use percentages

--- js/manage.js
+++ js/manage.js
@@ -24,26 +24,36 @@ $(document).ready(function() {
 
     function populateStress(data) {
       var stress = Sheetsee.getOccurance(data, "stress");
-      var betterStress = {"Not Stressed": stress["1"], "Stressed":stress["2"], "Very Stressed":stress["3"], "Hopeless":stress["4"]};
+      var total = data.length;
+      var betterStress = {"Not Stressed": percent(stress["1"], total), "Stressed":percent(stress["2"], total), "Very Stressed":percent(stress["3"], total), "Hopeless":percent(stress["4"], total)};
       var stressColors = Sheetsee.makeColorArrayOfObject(betterStress, colors);
       var options = {
         units: "units",
         m: [0, 0, 0, 0],
-        w: 400,
+        w: 500,
         h: 267,
         div: "#stress"
       }
       Sheetsee.d3PieChart(stressColors, options);
     }
 
+    function percentage(value, total) {
+      return value/total;
+    }
+
+    function percent(value, total) {
+      return (percentage(value, total)*100).toFixed(0);
+    }
+
     function populateCategory(data) {
       var category = Sheetsee.getOccurance(data, "filter");
-      var betterCategory = {"Paying Bills": category["bills"], "Saving Money": category["savings"]};
+      var total = data.length;
+      var betterCategory = {"Paying Bills": percent(category["bills"], total), "Saving Money": percent(category["savings"], total)};
       var categoryColors = Sheetsee.makeColorArrayOfObject(betterCategory, colors);
       var options = {
         units: "units",
         m: [0, 0, 0, 0],
-        w: 400,
+        w: 500,
         h: 267,
         div: "#category"
       }
@@ -53,7 +63,10 @@ $(document).ready(function() {
     function populateBarGraph(data, filter) {
       var filteredData = Sheetsee.getMatches(data, filter, "filter");
       var level = Sheetsee.getOccurance(filteredData, "level");
-      var betterLevels = [{frequency: "Never", total: level["1"], s: 1}, {frequency: "Couple Times", total: level["2"], s: 2}, {frequency: "Monthly", total: level["3"], s: 3}];
+      var never = getLevelStress(filteredData, "1", "Never");
+      var couple = getLevelStress(filteredData, "2", "Couple Times");
+      var monthly = getLevelStress(filteredData, "3", "Monthly");
+      var betterLevels = never.concat(couple).concat(monthly);
 
       var svg = dimple.newSvg("#" + filter, 400, 271);
       var barChart = new dimple.chart(svg, betterLevels);
@@ -62,8 +75,22 @@ $(document).ready(function() {
       x.title = "";
       var y = barChart.addMeasureAxis("y", "total");
       y.title = "respondents";
-      barChart.addSeries(null, dimple.plot.bar);
+      y.tickFormat ="%";
+      barChart.addSeries("stress", dimple.plot.bar);
       barChart.defaultColors = colors.map(function(hex){ return new dimple.color(hex); });
+      barChart.addLegend(0, 10, 410, 20, "right");
       barChart.draw();
     };
+
+    function getLevelStress(data, level, levelName) {
+      var filterByLevel = Sheetsee.getMatches(data, level, "level");
+      var stress = Sheetsee.getOccurance(filterByLevel, "stress");
+      var total = filterByLevel.length;
+      var not = {stress: "Not Stressed", frequency: levelName, total: percentage(stress["1"], total), s: level};
+      var some = {stress: "Stressed", frequency: levelName, total: percentage(stress["2"], total), s: level};
+      var very = {stress: "Very Stressed", frequency: levelName, total: percentage(stress["3"], total), s: level};
+      var hopeless = {stress: "Hopeless", frequency: levelName, total: percentage(stress["4"], total), s: level};
+      return [not, some, very, hopeless];
+    }
+
 }) //close ready
