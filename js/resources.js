86f3e3c44d6d390290048f81cda268109d6d454c
Becky Boone <becky.sue.boone@gmail.com>
2015-10-19 14:15:02 -0600

    Add resources page
    Add table with filters. Use material style.

--- js/resources.js
+++ js/resources.js
@@ -1,19 +1,41 @@
-  <script type="text/javascript">
-    document.addEventListener('DOMContentLoaded', function() {
-        var URL = "https://docs.google.com/spreadsheets/d/1hcZfADUtL0NKFJoY3TSSuYIbUy9W4xTT22fu1uHX52I/pubhtml"
-        Tabletop.init( { key: URL, callback: myData, simpleSheet: true } )
-    })
-    function myData(data) {
-          var tableOptions = {
-              "data": data,
-              "pagination": 10,
-              "filterDiv": "#placeholderFilter",
-              "tableDiv": "#placeholder" 
-              }
-  
-		  Sheetsee.makeTable(tableOptions)
-		  Sheetsee.initiateTableFilter(tableOptions)
-		  
-		  console.log(data);
-    }
-  </script>
+ $(document).ready(function() {
+   var URL = "https://docs.google.com/spreadsheets/d/1sTasDpO_Ay2YLFLFGgrIOQ9KYzB5SK3DU5mKBmcK6sE/pubhtml?gid=1856021763&single=true";
+   var cfpbColumn = "CFPB Tags (All, FF, FS, PF, PS)";
+
+   Tabletop.init({
+     key: URL,
+     callback: showData,
+     simpleSheet: true
+   });
+
+   function showData(data, tabletop) {
+     var tableOptions = {
+      "data": data,
+      "pagination": 10,
+      "tableDiv": "#resources",
+     };
+
+     Sheetsee.makeTable(tableOptions);
+
+     $("input[type=checkbox]").change(function() {
+       var filtered = [];
+       var checked = $("input[type=checkbox]:checked");
+       if(checked) {
+         data.forEach(function(d){
+           var tags = d[cfpbColumn].replace(/\s+/g, "").split(',');
+           var found = false;
+           $.each(checked, function(index, cb){
+             if(tags.indexOf(cb.id.trim()) > -1 && !found) {
+               found = true
+               filtered.push(d);
+             }
+           });
+         });
+         Sheetsee.makeTable(tableOptions, filtered);
+
+       } else {
+         Sheetsee.makeTable(tableOptions);
+       }
+     });
+   }
+ }) //close ready
